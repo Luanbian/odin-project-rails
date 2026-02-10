e25fb218549c41881ce2c41253c89596ca3866f6
luanbianchini <luanbianchine1@gmail.com>
2026-02-10 12:09:37 -0300

    fix: resolve migration pending after scaffold

--- store-app/db/schema.rb
+++ store-app/db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_10_144152) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_10_150723) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
 
+  create_table "products", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.string "sku"
+    t.integer "stock"
+    t.integer "stock_status"
+    t.datetime "updated_at", null: false
+  end
 end
