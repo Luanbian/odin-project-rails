a2ad1502ff3779245898d6c42e7b713f157dd15a
luanbianchini <luanbianchine1@gmail.com>
2026-02-10 12:47:36 -0300

    feat: validate and calculate stock status

--- store-app/app/controllers/products_controller.rb
+++ store-app/app/controllers/products_controller.rb
@@ -65,6 +65,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.expect(product: [ :sku, :name, :stock, :stock_status ])
+      params.expect(product: [ :name, :stock ])
     end
 end
--- store-app/app/models/product.rb
+++ store-app/app/models/product.rb
@@ -1,2 +1,22 @@
 class Product < ApplicationRecord
+  enum :stock_status, { in_stock: 0, low_stock: 1, out_of_stock: 2 }
+
+  before_validation :generate_sku, on: :create
+  before_save :calculate_stock_status
+
+  private
+    def generate_sku
+      self.sku ||= "SKU-#{name.parameterize.downcase}-#{SecureRandom.hex(2)}" if name.present?
+    end
+
+    def calculate_stock_status
+      self.stock_status = case stock
+      when ..0
+        :out_of_stock
+      when 1..10
+        :low_stock
+      else
+        :in_stock
+      end
+    end
 end
