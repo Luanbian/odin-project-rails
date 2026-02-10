6d2160a60f1862a6865d22f154b877b818494942
luanbianchini <luanbianchine1@gmail.com>
2026-02-10 12:07:58 -0300

    feat: scaffold product

--- store-app/config/application.rb
+++ store-app/config/application.rb
@@ -15,6 +15,7 @@ module StoreApp
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    config.active_record.primary_key = :uuid
 
     # Configuration for the application, engines, and railties goes here.
     #
